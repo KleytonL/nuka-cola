1644f6d4722618178026add961f05e2935727d4b
FoulHunter <kleytonluizdesouzavieira@gmail.com>
2025-03-18 11:50:48 -0300

    save-userinput

--- scripts/events.js
+++ scripts/events.js
@@ -1,2 +1,13 @@
-const btnNext = document.getElementById('next');
-const btnPrevious = document.getElementById('previous');
+function saveInput() {
+    const input = document.getElementById('username');
+    sessionStorage.setItem("user", input.value);
+    console.log(input.value);
+}
+
+function printInput() {
+    const storedName = sessionStorage.getItem('user');
+    var title = document.getElementById('helloUser');
+    title.innerText += " " + storedName + "!";
+}
+
+window.onload = printInput();
